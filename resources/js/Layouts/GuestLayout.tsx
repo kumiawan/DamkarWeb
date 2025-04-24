f5f90f56919c34739317bbf6af13d19a178d62a1
kurniawan <fajard916@gmail.com>
2025-04-24 20:08:39 +0700

    UPDATE: login+register

---
+++ resources/js/Layouts/GuestLayout.tsx
@@ -0,0 +1,10 @@
+import { Link } from '@inertiajs/react';
+import { PropsWithChildren } from 'react';
+
+export default function Guest({ children }: PropsWithChildren) {
+    return (
+        <div className="min-h-screen grid justify-center items-center sm:flex sm:justify-around sm:items-center bg-gray-100">
+                {children}
+        </div>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,22 @@
 <?php
 
-use App\Http\Controllers\MonitoringController;
-use App\Http\Controllers\NavbarController;
-use App\Http\Controllers\GeoJsonController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::get('/', [NavbarController::class, 'index']);
-Route::get('/monitoring', [MonitoringController::class, 'index'])->name('monitoring');
-Route::get('/geojson-data', [GeoJsonController::class, 'getGeoJsonData']);
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
